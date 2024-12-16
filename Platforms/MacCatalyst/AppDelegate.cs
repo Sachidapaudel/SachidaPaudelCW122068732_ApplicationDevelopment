bb0fa72073b105812a1cde3125c005f5d1f31ba2
Sachida Paudel <paudelsachida@gmail.com>
2024-12-16 10:16:35 +0545

    all changes

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SachidaPaudel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
